74626ce2c741058f4b41228edd426dbed665f561
GVDmeijde <matau_strong@hotmail.com>
2025-01-12 12:14:17 +0100

    Cron cant start from entrypoint script envsubst has too many arguments
    Signed-off-by: GVDmeijde <matau_strong@hotmail.com>

--- SatisfactoryMap/satisfactory-mapview/entrypoint.sh
+++ SatisfactoryMap/satisfactory-mapview/entrypoint.sh
@@ -1,12 +1,9 @@
 #!/bin/sh
 
 echo "Creating nginx config for: ${SCHEME}://${DOMAIN_NAME}"
-envsubst \$DOMAIN_NAME \$SCHEME < /nginx.conf.tmpl > /etc/nginx/nginx.conf
+envsubst \$DOMAIN_NAME,\$SCHEME < /nginx.conf.tmpl > /etc/nginx/nginx.conf
 
 echo "Starting NGinx server..."
 nginx-debug '-g' 'daemon off;'
-
-echo "Starting cron job..."
-cron '-f'
 
 echo "Container started!"
