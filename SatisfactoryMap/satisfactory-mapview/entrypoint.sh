35d50950825f9d5c140b1a8fe94a6c178f2b134d
GVDmeijde <matau_strong@hotmail.com>
2025-01-12 12:17:38 +0100

    atempting to fix envsubst syntax
    Signed-off-by: GVDmeijde <matau_strong@hotmail.com>

--- SatisfactoryMap/satisfactory-mapview/entrypoint.sh
+++ SatisfactoryMap/satisfactory-mapview/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 echo "Creating nginx config for: ${SCHEME}://${DOMAIN_NAME}"
-envsubst \$DOMAIN_NAME,\$SCHEME < /nginx.conf.tmpl > /etc/nginx/nginx.conf
+envsubst '$DOMAIN_NAME,$SCHEME' < /nginx.conf.tmpl > /etc/nginx/nginx.conf
 
 echo "Starting NGinx server..."
 nginx-debug '-g' 'daemon off;'
