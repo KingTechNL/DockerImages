74be9f913dc553c8a02783b57aefc3bb6deed4f4
GVDmeijde <matau_strong@hotmail.com>
2025-01-12 11:44:29 +0100

    Added some logging
    Signed-off-by: GVDmeijde <matau_strong@hotmail.com>

--- SatisfactoryMap/satisfactory-mapview/entrypoint.sh
+++ SatisfactoryMap/satisfactory-mapview/entrypoint.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+echo "Creating nginx config for: ${SCHEME}://${DOMAIN_NAME}"
 envsubst \$DOMAIN_NAME,\$SCHEME < /nginx.conf.tmpl > /etc/nginx/nginx.conf
+
+echo "Starting NGinx server..."
 nginx-debug '-g' 'daemon off;'
+
+echo "Starting cron job..."
 cron '-f'
+
+echo "Container started!"
