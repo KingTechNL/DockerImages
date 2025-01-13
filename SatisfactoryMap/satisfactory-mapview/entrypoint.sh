8c21d98fa4dc28569c9bb95d868f725c71fe600c
GVDmeijde <matau_strong@hotmail.com>
2025-01-13 09:55:42 +0100

    typo
    Signed-off-by: GVDmeijde <matau_strong@hotmail.com>

--- SatisfactoryMap/satisfactory-mapview/entrypoint.sh
+++ SatisfactoryMap/satisfactory-mapview/entrypoint.sh
@@ -4,7 +4,7 @@ echo "Creating nginx config for: ${SCHEME}://${USER_DOMAIN}"
 envsubst '$USER_DOMAIN,$SCHEME,$SAVE_NAME' < /nginx.conf.tmpl > /etc/nginx/nginx.conf
 
 echo "Setting symlink name to '${SAVE_NAME}'"
-envsubst '$SAVE_NAME' < /update-save.sh.tmpl ? /update-save.sh
+envsubst '$SAVE_NAME' < /update-save.sh.tmpl > /update-save.sh
 chmod +x /update-save.sh
 
 echo "Starting cron..."
