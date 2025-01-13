72a0f96d146419a4373d05b91be0e4f95300ac6b
GVDmeijde <matau_strong@hotmail.com>
2025-01-13 09:52:50 +0100

    Need to make new update_save executable at runtime
    Signed-off-by: GVDmeijde <matau_strong@hotmail.com>

--- SatisfactoryMap/satisfactory-mapview/entrypoint.sh
+++ SatisfactoryMap/satisfactory-mapview/entrypoint.sh
@@ -5,6 +5,7 @@ envsubst '$USER_DOMAIN,$SCHEME,$SAVE_NAME' < /nginx.conf.tmpl > /etc/nginx/nginx
 
 echo "Setting symlink name to '${SAVE_NAME}'"
 envsubst '$SAVE_NAME' < /update-save.sh.tmpl ? /update-save.sh
+chmod +x /update-save.sh
 
 echo "Starting cron..."
 cron
