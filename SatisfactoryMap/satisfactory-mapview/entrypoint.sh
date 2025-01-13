2956dca2d7711068bb624616ccbd8c57d819e6fa
GVDmeijde <matau_strong@hotmail.com>
2025-01-13 09:42:12 +0100

    Making SAVE_NAME configurable
    Signed-off-by: GVDmeijde <matau_strong@hotmail.com>

--- SatisfactoryMap/satisfactory-mapview/entrypoint.sh
+++ SatisfactoryMap/satisfactory-mapview/entrypoint.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
 echo "Creating nginx config for: ${SCHEME}://${USER_DOMAIN}"
-envsubst '$USER_DOMAIN,$SCHEME' < /nginx.conf.tmpl > /etc/nginx/nginx.conf
+envsubst '$USER_DOMAIN,$SCHEME,$SAVE_NAME' < /nginx.conf.tmpl > /etc/nginx/nginx.conf
+
+echo "Setting symlink name to '${SAVE_NAME}'"
+envsubst '$SAVE_NAME' < /nginx.conf.tmpl > /etc/nginx/nginx.conf
 
 echo "Starting cron..."
 cron
--- SatisfactoryMap/satisfactory-mapview/update-save.sh
+++ SatisfactoryMap/satisfactory-mapview/update-save.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-latest_file=$(ls -t /saves | grep -v 'latest.sav' | head -n 1)
+latest_file=$(ls -t /saves | grep -v '${SAVE_NAME}' | head -n 1)
 
 current_datetime=$(date +"[%Y-%m-%d %H:%M:%S]")
 
-out_ln=$(ln -vsf "./$latest_file" /saves/latest.sav)
+out_ln=$(ln -vsf "./$latest_file" /saves/${SAVE_NAME})
 
 echo "$current_datetime $out_ln"
