2c72245019b37b4f6417193fd3143eab4a9aaf25
GVDmeijde <matau_strong@hotmail.com>
2025-01-13 09:49:57 +0100

    copy paste issue
    Signed-off-by: GVDmeijde <matau_strong@hotmail.com>

--- SatisfactoryMap/satisfactory-mapview/entrypoint.sh
+++ SatisfactoryMap/satisfactory-mapview/entrypoint.sh
@@ -4,7 +4,7 @@ echo "Creating nginx config for: ${SCHEME}://${USER_DOMAIN}"
 envsubst '$USER_DOMAIN,$SCHEME,$SAVE_NAME' < /nginx.conf.tmpl > /etc/nginx/nginx.conf
 
 echo "Setting symlink name to '${SAVE_NAME}'"
-envsubst '$SAVE_NAME' < /nginx.conf.tmpl > /etc/nginx/nginx.conf
+envsubst '$SAVE_NAME' < /update-save.sh
 
 echo "Starting cron..."
 cron
